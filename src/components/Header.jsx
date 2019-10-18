e772d43dfa24b1dcdbe4f9f3290ab2c49ad83ac3
Jose Manuel Salazar <soltisam@gmail.com>
2019-10-18 13:50:19 -0500

    change title mydate for easydate

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ const  Header = () => {
   return (
     <header>
       <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 bg-white shadow-sm">
-        <h5 class="my-0 mr-md-auto font-weight-normal">MyDate</h5>
+        <h5 class="my-0 mr-md-auto font-weight-normal">EasyDate</h5>
           <nav class="my-2 my-md-0 mr-md-3">
             <a class="p-2 text-dark" href="/#">Conocenos</a>
             <a class="p-2 text-dark" href="/#">Planes</a>
